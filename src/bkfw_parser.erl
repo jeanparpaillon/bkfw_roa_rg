224003ab037f4dfaba66f3bf7be8f894d70f88c2
Jean Parpaillon <jean.parpaillon@free.fr>
2014-10-23 18:06:38 +0200

    Transform parsed command in atom

--- src/bkfw_parser.erl
+++ src/bkfw_parser.erl
@@ -26,9 +26,19 @@ parse_cmd({error, Err}, _) ->
 parse_cmd(eof, _) ->
     {error, eof};
 parse_cmd({ok, {string, Cmd}, <<>>}, Addr) ->
-    {ok, Addr, Cmd, []};
+    case kw_to_atom(Cmd) of
+	{error, Err} -> 
+	    {error, Err};
+	AtomCmd ->
+	    {ok, Addr, AtomCmd, []}
+    end;
 parse_cmd({ok, {string, Cmd}, Rest}, Addr) ->
-    parse_args(bkfw_scanner:token(Rest), Addr, Cmd, []);
+    case kw_to_atom(Cmd) of
+	{error, Err} -> 
+	    {error, Err};
+	AtomCmd ->
+	    parse_args(bkfw_scanner:token(Rest), Addr, AtomCmd, [])
+    end;
 parse_cmd({ok, {Tok, _}, _Rest}, _A) ->
     {error, io_lib:format("Expect string, got: ~p", [Tok])}.
 
@@ -39,3 +49,36 @@ parse_args(eof, A, C, Args) ->
     {ok, A, C, lists:reverse(Args)};
 parse_args({ok, {_Type, Arg}, Rest}, A, C, Args) ->
     parse_args(bkfw_scanner:token(Rest), A, C, [ Arg | Args ]).
+
+kw_to_atom(<<"RCC">>)       -> rcc;
+kw_to_atom(<<"CC">>)        -> cc;
+kw_to_atom(<<"SCC">>)       -> scc;
+kw_to_atom(<<"RGC">>)       -> rgc;
+kw_to_atom(<<"GC">>)        -> gc;
+kw_to_atom(<<"SGC">>)       -> sgc;
+kw_to_atom(<<"RPC">>)       -> rpc;
+kw_to_atom(<<"PC">>)        -> pc;
+kw_to_atom(<<"SPC">>)       -> spc;
+kw_to_atom(<<"RMODE">>)     -> rmode;
+kw_to_atom(<<"MODE">>)      -> mode;
+kw_to_atom(<<"OFF">>)       -> off;
+kw_to_atom(<<"SMODE">>)     -> smode;
+kw_to_atom(<<"RA">>)        -> ra;
+kw_to_atom(<<"ALARMS">>)    -> alarms;
+kw_to_atom(<<"PIN">>)       -> pin;
+kw_to_atom(<<"POUT">>)      -> pout;
+kw_to_atom(<<"PUMP_TEMP">>) -> pump_temp;
+kw_to_atom(<<"PUMP_BIAS">>) -> pump_bias;
+kw_to_atom(<<"EDFA_TEMP">>) -> edfa_temp;
+kw_to_atom(<<"EDFA_PSU">>)  -> edfa_psu;
+kw_to_atom(<<"BREF">>)      -> bref;
+kw_to_atom(<<"ADI">>)       -> adi;
+kw_to_atom(<<"MUTE">>)      -> mute;
+kw_to_atom(<<"RLT">>)       -> rlt;
+kw_to_atom(<<"LT">>)        -> lt;
+kw_to_atom(<<"RLC">>)       -> rlc;
+kw_to_atom(<<"LC">>)        -> lc;
+kw_to_atom(<<"RIT">>)       -> rit;
+kw_to_atom(<<"IT">>)        -> it;
+kw_to_atom(<<"RI">>)        -> ri;
+kw_to_atom(_S) -> {error, io_lib:format("Invalid keyword: ~p", [_S])}.
