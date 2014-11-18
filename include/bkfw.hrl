9b4d36a9fcebe7ed72c6435c44a1682799d3d48e
Jean Parpaillon <jean.parpaillon@free.fr>
2014-11-18 11:46:50 +0100

    Add debug macro

--- include/bkfw.hrl
+++ include/bkfw.hrl
@@ -1,3 +1,13 @@
+-define(DEBUG, true).
+
+-ifdef(DEBUG).
+-define(debug(Msg), io:format("DEBUG: " ++ Msg)).
+-define(debug(Msg, Data), io:format("DEBUG: " ++ Msg, Data)).
+-else.
+-define(debug(Msg), true).
+-define(debug(Msg, Data), true).
+-endif.
+
 -define(info(Msg), io:format("INFO: " ++ Msg)).
 -define(info(Msg, Data), io:format("INFO: " ++ Msg, Data)).
 
