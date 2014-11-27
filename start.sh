48e3c7b051060348229d12fa4377923c4a4a1286
Jean Parpaillon <jean.parpaillon@free.fr>
2014-11-27 10:48:26 +0100

    Add user settings conf file

--- start.sh
+++ start.sh
@@ -5,6 +5,12 @@ ebindir=${BASEDIR}/ebin
 depsdir=${BASEDIR}/deps/*/ebin
 reldir=${BASEDIR}/rel
 
+USERCONF=/var/tmp/bkfw_user.conf
+
+if [ ! -e /var/tmp/bkfw_user.confif ]; then
+    echo "[]." > /var/tmp/bkfw_user.config
+fi
+
 erl -sname agent \
     -pa ${ebindir} \
     -pa ${depsdir} \
