ef0887afa7a38055232352abf2a867e034b79062
Jean Parpaillon <jean.parpaillon@free.fr>
2015-02-01 12:17:18 +0100

    Fix indices in traps

--- src/bkfw_alarms_snmp.erl
+++ src/bkfw_alarms_snmp.erl
@@ -63,17 +63,51 @@ alarm_to_trap(adi, _) -> ampAdiTrap;
 alarm_to_trap(mute, _) -> ampMuteTrap;
 alarm_to_trap(_, _) -> undefined.
 
-alarm_to_vars(pin, #ampTable{index=Idx}=E) ->                        [{ampPowerPd1,     [Idx], round(E#ampTable.powerPd1)}];
-alarm_to_vars(pout, #ampTable{index=Idx}=E) ->                       [{ampPowerPd2,     [Idx], round(E#ampTable.powerPd2)}];
-alarm_to_vars(pump_temp, #ampTable{index=Idx}=E) ->                  [{ampCurLaserTemp, [Idx], round(E#ampTable.curLaserTemp)}];
-alarm_to_vars(pump_bias, #ampTable{index=Idx}=E) ->                  [{ampCurAmp,       [Idx], round(E#ampTable.curAmp)}];
-alarm_to_vars(edfa_temp, #ampTable{index=Idx, curInternalTemp=T}) -> [{ampCurInternalTemp, [Idx], round(T)}];
-alarm_to_vars(edfa_psu, #ampTable{index=Idx, powerSupply=P}) ->      [{ampPowerSupply, [Idx], round(P)}];
-alarm_to_vars(psu1, {_, _}) ->                                       [];
-alarm_to_vars(psu2, {_, _}) ->                                       [];
-alarm_to_vars(edfa_temp, {IT, _}) ->                                 [{smmCurInternalTemp, round(IT)}];
-alarm_to_vars(edfa_psu, {_, PS}) ->                                  [{smmPowerSupply, round(PS)}];
-alarm_to_vars(bref, #ampTable{index=Idx}=E) ->                       [{ampPowerPd2, [Idx], round(E#ampTable.powerPd2)},
-								      {ampPowerPd3, [Idx], round(E#ampTable.powerPd3)}];
-alarm_to_vars(adi, #ampTable{index=Idx}) ->                          [{ampIndex, Idx}];
-alarm_to_vars(mute, #ampTable{index=Idx}) ->                         [{ampIndex, Idx}].
+alarm_to_vars(pin, #ampTable{index=Idx}=E) ->                        
+    [
+     {ampIndex,        [Idx], Idx},
+     {ampPowerPd1,     [Idx], round(E#ampTable.powerPd1)}
+    ];
+alarm_to_vars(pout, #ampTable{index=Idx}=E) ->                       
+    [
+     {ampIndex,        [Idx], Idx},
+     {ampPowerPd2,     [Idx], round(E#ampTable.powerPd2)}
+    ];
+alarm_to_vars(pump_temp, #ampTable{index=Idx}=E) ->                  
+    [
+     {ampIndex,        [Idx], Idx},     
+     {ampCurLaserTemp, [Idx], round(E#ampTable.curLaserTemp)}
+    ];
+alarm_to_vars(pump_bias, #ampTable{index=Idx}=E) ->
+    [
+     {ampIndex,        [Idx], Idx},
+     {ampCurAmp,       [Idx], round(E#ampTable.curAmp)}
+    ];
+alarm_to_vars(edfa_temp, #ampTable{index=Idx, curInternalTemp=T}) ->
+    [
+     {ampIndex, [Idx], Idx},
+     {ampCurInternalTemp, [Idx], round(T)}
+    ];
+alarm_to_vars(edfa_psu, #ampTable{index=Idx, powerSupply=P}) ->      
+    [
+     {ampIndex, [Idx], Idx},
+     {ampPowerSupply, [Idx], round(P)}
+    ];
+alarm_to_vars(psu1, {_, _}) ->                                       
+    [];
+alarm_to_vars(psu2, {_, _}) ->                                       
+    [];
+alarm_to_vars(edfa_temp, {IT, _}) ->                                 
+    [{smmCurInternalTemp, round(IT)}];
+alarm_to_vars(edfa_psu, {_, PS}) ->                                  
+    [{smmPowerSupply, round(PS)}];
+alarm_to_vars(bref, #ampTable{index=Idx}=E) ->                       
+    [
+     {ampIndex, [Idx], Idx},
+     {ampPowerPd2, [Idx], round(E#ampTable.powerPd2)},
+     {ampPowerPd3, [Idx], round(E#ampTable.powerPd3)}
+    ];
+alarm_to_vars(adi, #ampTable{index=Idx}) ->                          
+    [{ampIndex, [Idx], Idx}];
+alarm_to_vars(mute, #ampTable{index=Idx}) ->                         
+    [{ampIndex, [Idx], Idx}].
