ec89fafa02f903864d8cd0ac1e55577f1af0472c
Jean Parpaillon <jean.parpaillon@free.fr>
2015-06-10 20:47:00 +0200

    Fix indent

--- src/bkfw_srv.erl
+++ src/bkfw_srv.erl
@@ -3,11 +3,11 @@
 
 -include("bkfw.hrl").
 
-% API
+												% API
 -export([start_link/0,
-	 command/3]).
+		 command/3]).
 
-% Internal
+												% Internal
 -export([init/0]).
 
 -define(FSM, ?MODULE).
@@ -26,73 +26,73 @@ command(Idx, Cmd, Args) when is_integer(Idx), is_atom(Cmd) ->
     CmdName = string:to_upper(atom_to_list(Cmd)),
     Timeout = application:get_env(bkfw, timeout, ?TIMEOUT),
     ArgsStr = case Args of
-		  [] -> "";
-		  _ -> [" ", Args]
-	      end,
+				  [] -> "";
+				  _ -> [" ", Args]
+			  end,
     Ref = make_ref(),
     ?FSM ! {wait, self(), Ref},
     receive
-	{com, Com} ->
-	    bkfw_com:send(Com, Idx, [CmdName, ArgsStr]),
-	    wait_answer(Idx, cmd_to_ans(Cmd), Ref, Timeout)
+		{com, Com} ->
+			bkfw_com:send(Com, Idx, [CmdName, ArgsStr]),
+			wait_answer(Idx, cmd_to_ans(Cmd), Ref, Timeout)
     after Timeout ->
-	    ?FSM ! {signal, Ref},
-	    {error, timeout}
+			?FSM ! {signal, Ref},
+			{error, timeout}
     end.
 
 wait_answer(Idx, '_', Ref, T) ->
     receive
-	{error, Err} ->
-	    ?FSM ! {signal, Ref},
-	    {error, Err};
-	{msg, {Idx, _, _}=Msg} -> 
-	    ?FSM ! {signal, Ref},
-	    {ok, Msg}
+		{error, Err} ->
+			?FSM ! {signal, Ref},
+			{error, Err};
+		{msg, {Idx, _, _}=Msg} -> 
+			?FSM ! {signal, Ref},
+			{ok, Msg}
     after T -> 
-	    ?FSM ! {signal, Ref},
-	    {error, timeout}
+			?FSM ! {signal, Ref},
+			{error, timeout}
     end;    
 wait_answer(Idx, Ans, Ref, T) ->
     receive
-	{error, Err} ->
-	    ?FSM ! {signal, Ref},
-	    {error, Err};
-	{msg, {Idx, Ans, _}=Msg} -> 
-	    ?FSM ! {signal, Ref},
-	    {ok, Msg}
+		{error, Err} ->
+			?FSM ! {signal, Ref},
+			{error, Err};
+		{msg, {Idx, Ans, _}=Msg} -> 
+			?FSM ! {signal, Ref},
+			{ok, Msg}
     after T -> 
-	    ?FSM ! {signal, Ref},
-	    {error, timeout}
+			?FSM ! {signal, Ref},
+			{error, timeout}
     end.
 
 init() ->
     ?info("Starting command server", []),
     Dev = application:get_env(bkfw, com, undefined),
     case bkfw_com:start_link(Dev) of
-	{ok, Com} ->
-	    ?info("Command server started: ~p", [Com]),
-	    free(Com);
-	ignore ->
-	    ?error("Error starting COM port: ignore", []),
-	    ok;
-	{error, Err} -> 
-	    ?error("Error starting COM port: ~p", [Err]),
-	    ok
+		{ok, Com} ->
+			?info("Command server started: ~p", [Com]),
+			free(Com);
+		ignore ->
+			?error("Error starting COM port: ignore", []),
+			ok;
+		{error, Err} -> 
+			?error("Error starting COM port: ~p", [Err]),
+			ok
     end.
 
 free(Com) ->
     receive {wait, By, Ref} ->
-	    By ! {com, Com},
-	    busy(Com, Ref)
+			By ! {com, Com},
+			busy(Com, Ref)
     end.
 
 busy(Com, Ref) ->
     Timeout = application:get_env(bkfw, timeout, ?TIMEOUT),
     receive 
-	{signal, Ref} ->
-	    free(Com)
+		{signal, Ref} ->
+			free(Com)
     after Timeout ->
-	    free(Com)
+			free(Com)
     end.
 
 cmd_to_ans(rcc) -> cc;
