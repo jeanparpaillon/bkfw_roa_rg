1f407befb46b212018725d6790de11c6f71cf04e
Jean Parpaillon <jean.parpaillon@free.fr>
2016-05-23 11:37:15 +0200

    Fix indent

--- src/bkfw_index.erl
+++ src/bkfw_index.erl
@@ -8,16 +8,16 @@
 %% @private
 execute(Req, Env) ->
     case cowboy_req:method(Req) of
-	{<<"GET">>, Req1} -> check_url(Req1, Env);
-	{<<"HEAD">>, Req1} -> check_url(Req1, Env);
-	{<<"OPTIONS">>, Req1} -> check_url(Req1, Env);
-	{_M, Req1} -> {ok, Req1, Env}
+		{<<"GET">>, Req1} -> check_url(Req1, Env);
+		{<<"HEAD">>, Req1} -> check_url(Req1, Env);
+		{<<"OPTIONS">>, Req1} -> check_url(Req1, Env);
+		{_M, Req1} -> {ok, Req1, Env}
     end.
 
 check_url(Req, Env) ->
     case cowboy_req:path(Req) of
-	{<<"/">>, Req2} ->
-	    {ok, cowboy_req:set([{path, <<"/index.html">>}], Req2), Env};
-	{_, Req2} ->
-	    {ok, Req2, Env}
+		{<<"/">>, Req2} ->
+			{ok, cowboy_req:set([{path, <<"/index.html">>}], Req2), Env};
+		{_, Req2} ->
+			{ok, Req2, Env}
     end.
